8e877a42d249d3b554ced41016a94d080a9456e5
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2022-10-16 21:43:42 +0200

    Add files via upload

---
+++ 1.UkolDatumACas/1.UkolDatumACas/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace _1.UkolDatumACas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(253, 125);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(343, 215);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(329, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 93);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Kdo Je Nejstarší ?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(87, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(694, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Zadej osoby ve tvaru \"jméno;příjmení;datum narození\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(609, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "dd.mm.yyyy";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 586);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 1.UkolDatumACas/1.UkolDatumACas/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1.UkolDatumACas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool spravnedatum = true;
+            DateTime nejmensidatum = DateTime.Now;
+
+            double dny = 0;
+            double srovnani = 0;
+            int index = 0;
+            int indexpole = 0;
+
+            List<string> list = new List<string>();
+
+            foreach (string neco in textBox1.Lines)
+            {
+                try
+                {
+                string[] pepa = neco.Split(';');
+                string narozeni = pepa[2];
+                //string narozeni = neco.Substring(neco.LastIndexOf(';')+1,10);
+
+                DateTime narozenidate;
+                if (!DateTime.TryParse(narozeni, out narozenidate)) 
+                {
+                    MessageBox.Show("Nekde se stala chyba");
+                    spravnedatum = false;
+                    break;
+                }
+                narozenidate = DateTime.Parse(narozeni);
+
+                TimeSpan rozdil = (nejmensidatum - narozenidate);
+
+                dny = rozdil.TotalDays;
+
+                if (dny > srovnani)
+                {
+                    indexpole = index;
+                    srovnani = dny;
+                }
+
+                index++;
+                }
+                catch
+                {
+                    MessageBox.Show("chyba");
+                    spravnedatum = false;
+                    break;
+                }
+                
+            }
+
+            if(spravnedatum && dny > 0)
+            {
+                string[] neco = textBox1.Lines[indexpole].Split(';');
+                MessageBox.Show("Nejstarší je :" + neco[0] + " " + neco[1]);
+            }
+            textBox1.Text = "";
+
+        }
+    }
+}
